58cfddf024c5349c97701cac030c3b7b6f34db89
Admin <Admin@DESKTOP-H08TIEQ>
2018-06-13 15:56:38 +0300

    first commit

--- lab1_2/Controllers/HomeController.cs
+++ lab1_2/Controllers/HomeController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using lab1_2.Models;
 namespace lab1_2.Controllers
 {
     public class HomeController : Controller
@@ -12,7 +12,27 @@ namespace lab1_2.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult Index(Calculator am, String calc)
+        {
 
+            switch (calc)
+            {
+                case "Add":
+                    am.result = am.firstNum + am.secondNum;
+                    break;
+                case "Sub":
+                    am.result = am.firstNum - am.secondNum;
+                    break;
+                case "Mult":
+                    am.result = am.firstNum * am.secondNum;
+                    break;
+                case "Div":
+                    am.result = am.firstNum / am.secondNum;
+                    break;
+            }
+            return View(am);
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
---
+++ lab1_2/Models/Calculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace lab1_2.Models
+{
+    public class Calculator
+    {
+        public double firstNum { get; set; }
+        public double secondNum { get; set; }
+        public double result { get; set; }
+    }
+}
